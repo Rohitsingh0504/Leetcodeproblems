dc6dc94025d02e60ebc55a964c9f06a946533ad5
Rohit Singh <rohitsingh45672@gmail.com>
2026-07-30 13:28:02 +0530

    Time: 77 ms (23.27%), Space: 111.4 MB (26.05%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+
+        for(int n:nums)
+        {
+            mp[n]++;
+
+            if(mp[n]>1)
+            {
+                return true;
+            }
+
+        }
+        return false;
+    }
+};
